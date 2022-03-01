48d2cced9fd56e429892d25766e7334a6e7d371a
Matthew <imatty2011@hotmail.co.uk>
2022-03-01 21:23:14 +0000

    added anim to projects page

--- src/Components/ProjectCard/ProjectCard.tsx
+++ src/Components/ProjectCard/ProjectCard.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './ProjectCard.css';
 import Code from 'Components/Icons/Code';
 import Checkmark from 'Components/Icons/Checkmark';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 type Props = {
   img: string;
@@ -15,7 +16,7 @@ const ProjectCard = ({ img, buttonText, projectDesc }: Props) => {
     return buttonText.map((text) => <Button type="info" text={text} />);
   };
   return (
-    <section className="projectCard">
+    <ScrollAnimation className="projectCard" animateIn="fadeInUp">
       <img src={img} alt="project preview" />
       <div className="projectCard-inner__div">
         <h1>Shortly URL landing page</h1>
@@ -26,7 +27,7 @@ const ProjectCard = ({ img, buttonText, projectDesc }: Props) => {
           <Button type="action" text="Demo" img={<Checkmark />} />
         </div>
       </div>
-    </section>
+    </ScrollAnimation>
   );
 };
 
