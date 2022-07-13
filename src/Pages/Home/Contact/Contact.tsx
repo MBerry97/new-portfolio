64c717d2921915c84def0bbe7222dc89d60096cd
MBerry97 <imatty2011@hotmail.co.uk>
2022-07-13 16:04:00 +0100

    contact input now outline instead of box shadow

--- src/Pages/Home/Contact/Contact.tsx
+++ src/Pages/Home/Contact/Contact.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable react/destructuring-assignment */
 import Button from 'Components/Button/Button';
 import Send from 'Components/Icons/Send';
 import PageTitle from 'Components/Text/PageTitle/PageTitle';
